86cc6c3eae6555c0cf1ab01062f4d6232709d883
emarthkopp <emarth@outlook.com>
2023-10-23 13:20:24 +0200

    affichage galerie en dynamique

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/script.js
@@ -1,13 +1,24 @@
-fetch("http://localhost:5678/api/works")
+const url = "http://localhost:5678/api/works"
+const container = document.getElementById("gallery")
+
+
+fetch(url)
   .then(response => {
     // Gérer la réponse HTTP (conversion en JSON, gestion des erreurs, etc.)
+
     if (!response.ok) {
       throw new Error('Erreur de réseau : ' + response.status);
     }
-    return response.json(); // Si vous attendez une réponse JSON
+    return response.json(); 
   })
   .then(data => {
     // Traiter les données reçues
+    for(project in data){
+      container.innerHTML+=`<figure>
+      <img src=${data[project].imageUrl} alt=${data[project].title}>
+      <figcaption>${data[project].title}</figcaption>
+    </figure>`
+    }
     console.log(data);
   })
   .catch(error => {
