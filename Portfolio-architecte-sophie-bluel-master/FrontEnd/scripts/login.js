9afb9a828e46dc3585e3b6b3ef163caf7d6ecec0
emarthkopp <emarth@outlook.com>
2023-11-14 19:56:33 +0100

    création modale images projet

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/login.js
@@ -54,3 +54,5 @@ function login() {
   }
   
   login();
+
+  
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/script.js
@@ -1,11 +1,8 @@
-let token = localStorage.getItem('token')
-
-
 const container = document.getElementById("gallery")
 
 fetch("http://localhost:5678/api/works")
   .then(response => {
-    // Gérer la réponse HTTP (conversion en JSON, gestion des erreurs, etc.)
+    // Gérer la réponse HTTP (conversion en JSON, gestion des erreurs)
 
     if (!response.ok) {
       throw new Error('Erreur de réseau : ' + response.status);
@@ -13,13 +10,13 @@ fetch("http://localhost:5678/api/works")
     return response.json();
   })
   .then(data => {
-    this.projects=data;
+    projects = data;
     // Traiter les données reçues
-    for (project in data) {
+    for (works in data) {
 
       container.innerHTML += `<figure>
-      <img src=${data[project].imageUrl} alt=${data[project].title}>
-      <figcaption>${data[project].title}</figcaption>
+      <img src=${data[works].imageUrl} alt=${data[works].title}>
+      <figcaption>${data[works].title}</figcaption>
     </figure>`
     }
   })
@@ -28,71 +25,149 @@ fetch("http://localhost:5678/api/works")
     //console.error('Erreur lors de la requête Fetch :', error);
   });
 
-  const buttonObjects = document.getElementById("objects");
-  buttonObjects.addEventListener("click", function(){
-   
-    const objectsFiltres=projects.filter((project)=>
-      project.categoryId ==1
-    )
-    container.innerHTML = "";
-  for (project in objectsFiltres) {
+const buttonObjects = document.getElementById("objects");
+buttonObjects.addEventListener("click", function () {
+
+  const objectsFiltres = projects.filter((works) =>
+    works.categoryId == 1
+  )
+  container.innerHTML = "";
+  for (works in objectsFiltres) {
 
     container.innerHTML += `<figure>
-    <img src=${objectsFiltres[project].imageUrl} alt=${objectsFiltres[project].title}>
-    <figcaption>${objectsFiltres[project].title}</figcaption>
+    <img src=${objectsFiltres[works].imageUrl} alt=${objectsFiltres[works].title}>
+    <figcaption>${objectsFiltres[works].title}</figcaption>
   </figure>`
   }
-   })
-   const buttonFlats = document.getElementById("flats");
-  buttonFlats.addEventListener("click", function(){
-   
-    const flatsFiltres=projects.filter(function(project){
-      return project.categoryId ==2
+})
+const buttonFlats = document.getElementById("flats");
+buttonFlats.addEventListener("click", function () {
+
+  const flatsFiltres = projects.filter(function (works) {
+    return works.categoryId == 2
   })
   container.innerHTML = "";
-  for (project in flatsFiltres) {
+  for (works in flatsFiltres) {
 
     container.innerHTML += `<figure>
-    <img src=${flatsFiltres[project].imageUrl} alt=${flatsFiltres[project].title}>
-    <figcaption>${flatsFiltres[project].title}</figcaption>
+    <img src=${flatsFiltres[works].imageUrl} alt=${flatsFiltres[works].title}>
+    <figcaption>${flatsFiltres[works].title}</figcaption>
   </figure>`
   }
-   })
-  
-   const buttonHotels = document.getElementById("hotels");
-  buttonHotels.addEventListener("click", function(){
-   
-    const hotelsFiltres=projects.filter((project)=>
-      project.categoryId ==3
-    )
-    container.innerHTML = "";
-  for (project in hotelsFiltres) {
+})
+
+const buttonHotels = document.getElementById("hotels");
+buttonHotels.addEventListener("click", function () {
+
+  const hotelsFiltres = projects.filter((works) =>
+    works.categoryId == 3
+  )
+  container.innerHTML = "";
+  for (works in hotelsFiltres) {
 
     container.innerHTML += `<figure>
-    <img src=${hotelsFiltres[project].imageUrl} alt=${hotelsFiltres[project].title}>
-    <figcaption>${hotelsFiltres[project].title}</figcaption>
+    <img src=${hotelsFiltres[works].imageUrl} alt=${hotelsFiltres[works].title}>
+    <figcaption>${hotelsFiltres[works].title}</figcaption>
   </figure>`
   }
-   })
-   const buttonAll = document.getElementById("all");
-  buttonAll.addEventListener("click", function(){
-   
-    const allFiltres=projects.filter((project)=>
-      project
-    )
-    container.innerHTML = "";
-  for (project in allFiltres) {
+})
+const buttonAll = document.getElementById("all");
+buttonAll.addEventListener("click", function () {
+
+  const allFiltres = projects.filter((works) =>
+    works
+  )
+  container.innerHTML = "";
+  for (works in allFiltres) {
 
     container.innerHTML += `<figure>
-    <img src=${allFiltres[project].imageUrl} alt=${allFiltres[project].title}>
-    <figcaption>${allFiltres[project].title}</figcaption>
+    <img src=${allFiltres[works].imageUrl} alt=${allFiltres[works].title}>
+    <figcaption>${allFiltres[works].title}</figcaption>
   </figure>`
   }
-   });
-   
-   
-  
-   
-   
+});
 
-  
+
+if (localStorage.getItem('token')) {
+  let elementsConnected = document.querySelectorAll('.connected');
+
+  for (var i = 0; i < elementsConnected.length; i++) {
+    elementsConnected[i].style.display = "block";
+  }
+  let elemntsDisconnected = document.querySelectorAll('.disconnected');
+
+  for (var i = 0; i < elemntsDisconnected.length; i++) {
+    elemntsDisconnected[i].style.display = "none";
+  }
+}
+else {
+  let elementsConnected = document.querySelectorAll('.connected');
+
+  for (var i = 0; i < elementsConnected.length; i++) {
+    elementsConnected[i].style.display = "none";
+  }
+  let elemntsDisconnected = document.querySelectorAll('.disconnected');
+
+  for (var i = 0; i < elemntsDisconnected.length; i++) {
+    elemntsDisconnected[i].style.display = "block";
+  }
+}
+
+function logout() {
+  // Supprime l'ID et le token du localStorage
+  localStorage.removeItem('userId');
+  localStorage.removeItem('token');
+  // Redirige l'utilisateur vers la page en étatnt déconnecté
+  window.location.href = 'index.html';
+}
+
+addEventListener("hashchange", (event) => {
+  event.preventDefault();
+  const dest = event.target?.document?.location?.hash;
+  if (dest === "#logout") {
+    logout();
+  }
+});
+
+const modalModif = document.getElementById("modalModification");
+const btnOpenModal = document.getElementById("btnProjects");
+const btnCloseModal = document.getElementById("closeModal");
+const modalContent = document.getElementById("modalContent");
+
+modalModif.style.display = "none";
+
+btnOpenModal.onclick = function () {
+  modalModif.style.display = "block";
+  fetch("http://localhost:5678/api/works")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur : ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      modalContent.innerHTML = "";
+      for (works in data) {
+        modalContent.innerHTML += `<figure>
+          <img src=${data[works].imageUrl} alt=${data[works].title} >
+          <i class="fa-solid fa-trash-can" id="trash"></i>
+        </figure>`;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+btnCloseModal.onclick = function () {
+  modalModif.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+  if (event.target == modalModif) {
+    modalModif.style.display = "none";
+  }
+};
+
+
